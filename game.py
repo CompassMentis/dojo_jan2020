a0123332109a63359f0c2eb56a163b94510ad757
Coen de Groot <cdegroot@nomensa.com>
2020-01-15 19:58:58 +0000

    Start of landscape

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
 import pygame
 
 from settings import Settings
+from landscape import Landscape
 
+
+pygame.init()
 canvas = pygame.display.set_mode(Settings.screen_size)
 
+landscape = Landscape(canvas)
+
+clock = pygame.time.Clock()
+
+done = False
+while not done:
+    canvas.fill((0, 0, 0))
+    landscape.draw()
+    pygame.display.flip()
+    landscape.update()
+    clock.tick(10)
--- settings.py
+++ settings.py
@@ -2,3 +2,6 @@ class Settings:
     screen_width = 1800
     screen_height = 500
     screen_size = screen_width, screen_width
+    terrain_height = 50
+    terrain_colour = 0, 128, 0
+    terrain_base = screen_height - terrain_height
