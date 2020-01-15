713f55821223333bffe176fd2d7e573dfbd54359
Coen de Groot <cdegroot@nomensa.com>
2020-01-15 19:31:48 +0000

    Initial commit

---
+++ game.py
@@ -0,0 +1,6 @@
+import pygame
+
+from settings import Settings
+
+canvas = pygame.display.set_mode(Settings.screen_size)
+
---
+++ settings.py
@@ -0,0 +1,4 @@
+class Settings:
+    screen_width = 1800
+    screen_height = 500
+    screen_size = screen_width, screen_width
